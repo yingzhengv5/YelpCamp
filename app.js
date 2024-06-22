a90c3b3a571168aa278b1d127088bf574b402b59
Ying Zheng <zhengyingwhv@gmail.com>
2024-06-22 18:40:17 +1200

    Update app.js

--- app.js
+++ app.js
@@ -22,17 +22,15 @@ const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 const MongoStore = require('connect-mongo');
 
-// const dbUrl = process.env.DB_URL;
-
-const dbUrl = 'mongodb://127.0.0.1:27017/yelp-camp'
-mongoose.connect(dbUrl
+const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/yelp-camp';
+mongoose.connect(dbUrl);
     // , {
     // useNewUrlParser: true,
     // // useCreateIndex: true,
     // useUnifiedTopology: true,
     // // useFindAndModify: false
     // }
-);
+
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -52,12 +50,14 @@ app.use(
       replaceWith: '_',
     }),
 );
-  
+
+const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
+
 const store = MongoStore.create({
     mongoUrl: dbUrl,
     touchAfter: 24 * 60 * 60, //need to be seconds
     crypto: {
-        secret: 'thisshouldbeabettersecret!'
+        secret,
     }
 });
 
@@ -68,7 +68,7 @@ store.on('error', function (e) {
 const sessionConfig = {
     store,
     name: 'session',
-    secret: 'thisshouldbeabettersecret!',
+    secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
