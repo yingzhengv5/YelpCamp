df29e2beefcd1f8fe15c50cd6627b8a41ec0d137
Ying Zheng <zhengyingwhv@gmail.com>
2024-06-26 14:48:14 +1200

    fix bug - when user delete campground

--- middleware.js
+++ middleware.js
@@ -31,7 +31,7 @@ module.exports.validateCampground = (req, res, next) => {
 module.exports.isAuthor = async (req, res, next) => {
     const { id } = req.params;
     const campground = await Campground.findById(id);
-    if (!campground.author.equals(req.user._id)) {
+    if (!campground || !campground.author.equals(req.user._id)) {
         req.flash("error", "You do not have permission to do that!");
         return res.redirect(`/campgrounds/${id}`);
     }
